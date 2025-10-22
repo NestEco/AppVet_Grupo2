c7d2ed08bb3e3700823eb1fbcf77806664e1e075
RodrigoCarcamo1337 <ro.carcamom@duocuc.cl>
2025-10-22 00:44:03 -0300

    Arreglos y cambio de organizacion de botones en la pantalla de ReservarScreen.kt

--- app/src/main/java/com/example/appvet_grupo2/ui/screens/ReservarScreen.kt
+++ app/src/main/java/com/example/appvet_grupo2/ui/screens/ReservarScreen.kt
@@ -2,24 +2,27 @@ package com.example.appvet_grupo2.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.appvet_grupo2.navigation.Screen
 import com.example.appvet_grupo2.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,31 +30,69 @@ fun ReservarScreen(
     navController: NavController,
     viewModel: MainViewModel = viewModel()
 ) {
-    val scope = rememberCoroutineScope()
-
     Scaffold(
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF00AB66),
+                    titleContentColor = Color.White
+                ),
                 title = { Text("Elegir tipo de Agenda") },
             )
         }
     ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding)
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
+                .padding(innerPadding)
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            //Aquí sin importar cual se escoga, llevará a la pantalla FechaScreen, siendo la diferencia
+            //Aquí sin importar cual se escoja, llevará a la pantalla FechaScreen, siendo la diferencia
             //la variable que se almacena.
-            Button(onClick = {viewModel.navigateTo(Screen.SelectFecha)}) {
+            Button(
+                onClick = { viewModel.navigateTo(Screen.SelectFecha) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF00AB66),
+                    contentColor = Color.White
+                )
+            ) {
                 Text("Consulta")
             }
-            Button(onClick = {viewModel.navigateTo(Screen.SelectFecha)}) {
-                Text("Vacunacion y Deparacitacion")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { viewModel.navigateTo(Screen.SelectFecha) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF00AB66),
+                contentColor = Color.White
+            )
+            ) {
+                Text("Vacunacion y Desparacitacion")
             }
-            Button(onClick = {viewModel.navigateTo(Screen.SelectFecha)}) {
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { viewModel.navigateTo(Screen.SelectFecha) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF00AB66),
+                    contentColor = Color.White
+                )
+
+
+            ) {
                 Text("Cirugia y Procesos Especializados")
             }
         }
